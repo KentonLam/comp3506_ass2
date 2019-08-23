ac45a009181b309b171c3345958fe5cbc88ae0b0
Kenton Lam <kenton_lam@outlook.com>
2019-08-23 10:52:58 +1000

    improve q4 analysis, add comments, clean up code a bit

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,51 +1,64 @@
 import java.util.Queue;
 
 /**
- * Write your solution to this assignment in this class
+ * Solutions to COMP3506 assignment 2. Semester 2, 2019.
  */
 public class Algorithms {
 
     /**
-     * Write your implementation of the sortQueue algorithm here
+     * Given a queue of elements implementing the Comparable 
+     * interface, sorts the elements of the queue using O(1)
+     * extra space.
      *
-     * @param queue the queue to sort
+     * Dequeing the elements of the sorted queue will yield each
+     * element in ascending order.
+     *
+     * @param <T> Type of objects inside the queue.
+     * @param queue Queue to sort in-place.
      */
     public static <T extends Comparable<T>> void sortQueue(Queue<T> queue) {
-        if (queue.size() <= 1)
-            return; // nothing to do
         int size = queue.size();
+        if (size <= 1)
+            return; // nothing to do
+
         T a, b, prev;
         boolean repeat = true;
         while (repeat) {
             repeat = false;
-            //System.out.println("\niterating");
 
-            a = queue.remove();
+            a = queue.remove(); // pop element from queue
             b = null;
-            prev = null; // previously pushed element
+            prev = null; // current tail of the queue.
 
             for (int i = 0; i < size - 1; i++) {
+                // pop an element to fill either a or b, depending on which is
+                // null.
                 if (a == null) {
                     a = queue.remove();
                 } else {
                     b = queue.remove();
                 }
+
                 // toPush is the smaller of 'a' and 'b'.
                 T toPush = a.compareTo(b) < 0 ? a : b;
-                T other = toPush == a ? b : a;
-                queue.add(toPush); // push smaller to end of queue.
-                //System.out.println("pushing "+toPush+", keeping "+other);
+                // push smaller element to end of queue.
+                queue.add(toPush);
+
+                // clear reference to the element we pushed.
                 if (a == toPush) {
                     a = null;
                 } else {
                     b = null;
                 }
+
+                // if the previous tail of the queue is greater than the element
+                // we just pushed, something is out of order and we should repeat.
                 if (!repeat && prev != null && prev.compareTo(toPush) > 0) {
-                    //System.out.println("REPEAT FLAG SET");
                     repeat = true;
                 }
                 prev = toPush;
             }
+
             // add back the last element.
             queue.add(a != null ? a : b);
         }
@@ -54,6 +67,7 @@ public class Algorithms {
     /**
      * Computes the missing number in a given arithmetic sequence. The number
      * should be missing between numbers[start] and numbers[end].
+     *
      * @param numbers Arithmetic sequence with one missing term.
      * @param start Lowest bound of missing term's index.
      * @param end Upper bound of missing term's index.
@@ -85,18 +99,24 @@ public class Algorithms {
     }
 
     /**
-     * Write your implementation of the findMissingNumber algorithm here
+     * Given an array of integers representing an arithmetic sequence,
+     * finds the missing element.
+     *
+     * <br><b>Assumptions:</b>
+     * the missing element is not at the start or end of the sequence;
+     * the input array will contain at least two elements;
+     * exactly one element will be missing from the sequence.
      *
      * @param numbers the arithmetic sequence
      * @return the missing number in the sequence
      */
     public static int findMissingNumber(int[] numbers) {
-        int len = numbers.length-1;
-        // we expect len+1 'steps' in the sequence between the start and
+        int end = numbers.length-1;
+        // we expect end+1 steps in the sequence between the start and
         // end numbers, because one item is missing.
-        int delta = (numbers[len] - numbers[0]) / (len + 1);
+        int delta = (numbers[end] - numbers[0]) / (end + 1);
 
-        return findNumInRange(numbers, 0, len, delta);
+        return findNumInRange(numbers, 0, end, delta);
     }
 
 }
